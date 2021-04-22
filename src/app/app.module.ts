a00de55d88d4656775e7d42bf615876d1657dcd4
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2021-04-22 01:02:20 -0500

    provide bootstrap and font-awesome modules for angular 2.x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { UpgradeModule } from '@angular/upgrade/static';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import { SmartAcademyModule } from './smart-academy.module';
 
 import { InstitutionsModule } from './institutions/institutions.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     UpgradeModule,
+    NgbModule,
+    SharedModule,
 
     InstitutionsModule
   ]
 })
 export class AppModule {
-  constructor(private upgrade: UpgradeModule) { }
+  constructor(private upgrade: UpgradeModule, iconLibrary: FaIconLibrary) {
+    iconLibrary.addIconPacks(fas);
+  }
 
   ngDoBootstrap() {
     this.upgrade.bootstrap(document.body, [SmartAcademyModule.name], { strictDi: true });
--- src/app/institutions/institutions.module.ts
+++ src/app/institutions/institutions.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../shared/shared.module';
 import { InstitutionsComponent } from './institutions/institutions.component';
 import { DeleteModalComponent } from './delete-modal/delete-modal.component';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        SharedModule
     ],
     declarations: [
         InstitutionsComponent,
